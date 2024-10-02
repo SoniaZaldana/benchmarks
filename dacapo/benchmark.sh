65799700ece61f5e474c9b1691581388cc7feb88
SoniaZaldana <zaldanasonia@gmail.com>
2024-10-02 16:54:06 -0400

    Modifying benchmark script to support compressing object headers, specifying which garbage collector to use and specifying the number of times to run the entire benchmark

--- dacapo/benchmark.sh
+++ dacapo/benchmark.sh
@@ -1,62 +1,118 @@
-# !/bin/bash
+#!/bin/bash
 
-# cleanup
-rm -r -f logs scratch
+# Function to display usage information
+usage() {
+    echo "Usage: $0 -n <number_of_runs> [-parallel] [-compact]"
+    exit 1
+}
 
-# TODO update these to the proper Java version I'm measuring.
+# Default values
+num_runs=1
+parallel=false
+compact=false
+
+# Parse command line arguments
+while getopts ":n:p:c:" opt; do
+    case $opt in
+        n)
+            num_runs="$OPTARG"
+            ;;
+        p)
+            parallel=true
+            ;;
+        c)
+            compact=true
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            usage
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument." >&2
+            usage
+            ;;
+    esac
+done
+
+# Cleanup old directories
+rm -rf parallel g1
+
+# Java and DaCapo paths
 java="/home/szaldana/jdk/build/linux-x86_64-server-release/images/jdk/bin/java"
 dacapo="dacapo-23.11-chopin.jar"
 callback="../dacapocallback/target/dacapocallback-1.0-SNAPSHOT.jar"
-curr_time="`date +"%H-%M-%S_%Y-%m-%d"`"
 java_opts=""
-testing_opts="-XX:+UnlockExperimentalVMOptions -XX:-UseCompactObjectHeaders"
+testing_opts="-XX:+UnlockExperimentalVMOptions"
 
-# declare the benchmarks we want to run (all of them except h2o as that requires java versions <= 17)
- declare -a benchmarks=("avrora" "batik" "cassandra" "eclipse" "fop" "graphchi" "h2" "jme" "jython" "kafka" "luindex" "lusearch" "pmd" "spring" "sunflow" "tomcat" "tradebeans" "tradesoap" "xalan" "zxing")
+# Add options based on flags
+if $parallel; then
+    java_opts="$java_opts -XX:+UseParallelCollector"
+fi
 
-# TODO - remove. This is just so I don't have to do them all right now.
-#declare -a benchmarks=("batik")
+if $compact; then
+    java_opts="$java_opts -XX:+UseCompactObjectHeaders"
+fi
+
+# Print JAVA_OPTS and number of iterations
+echo "JAVA_OPTS: $java_opts"
+echo "Number of runs: $num_runs"
+
+# Set parent directory based on compact flag
+if $parallel; then
+    parent_dir="parallel"
+else
+    parent_dir="g1"
+fi
+
+# Create parent directory
+mkdir -p "$parent_dir"
+
+# Set subdirectory names based on compact flag
+if $compact; then
+    scratch_parent="$parent_dir/scratch_compact"
+    gc_parent="$parent_dir/logs_compact"
+else
+    scratch_parent="$parent_dir/scratch"
+    gc_parent="$parent_dir/logs"
+fi
+
+# Create scratch and logs directories
+mkdir -p "$scratch_parent"
+mkdir -p "$gc_parent"
+
+# Declare benchmarks
+declare -a benchmarks=("avrora" "batik" "cassandra" "eclipse" "fop" "graphchi" "h2" "jme" "jython" "kafka" "luindex" "lusearch" "pmd" "spring" "sunflow" "tomcat" "tradebeans" "tradesoap" "xalan" "zxing")
 
 # Build latest callback
 echo "Building latest callback version"
-cd ../dacapocallback
-mvn clean install
-cd ../dacapo
+(cd ../dacapocallback && mvn clean install)
 
-# create a directory to store gc logs and scratch data
-scratch_parent="scratch"
-gc_parent="logs"
-mkdir $scratch_parent
-mkdir $gc_parent
+# Run benchmarks and collect GC log files
+for bench in "${benchmarks[@]}"; do
+    for ((run=1; run<=num_runs; run++)); do
+        echo "******************* Running benchmark $bench - Run $run *******************"
 
-# Run the above benchmarks and collect gc log files
-for bench in "${benchmarks[@]}"
-do
-        echo "******************* Running benchmark $bench *******************"
-        if [ "$bench" = "h2" ] || [ "$bench" = "cassandra" ]
-        then
-                java_opts=" -Djava.security.manager=allow"
-        else
-                java_opts=""
+        # Set additional Java options based on benchmark type
+        if [[ "$bench" == "h2" || "$bench" == "cassandra" ]]; then
+            java_opts="$java_opts -Djava.security.manager=allow"
         fi
 
-        # Determine load size. Large for all unless unavailable.
+        # Determine load size
         size="large"
-        if [ "$bench" = "zxing" ] || [ "$bench" = "fop" ]
-        then
-                size="default"
+        if [[ "$bench" == "zxing" || "$bench" == "fop" ]]; then
+            size="default"
         fi
 
-        # Run the benchmark and put all the results in scratch directory.
-        # Also store gc logs.
-        scratch_dir=$scratch_parent/$bench
-        gc_file=$gc_parent/$bench.log
-        time_log=$gc_parent/$bench.time
-        mkdir $scratch_dir
-        touch $time_log
-        # TODO add -n 21 to stabilize the benchmark with 20 runs.
-        $java $java_opts $testing_opts -Xlog:gc*,metaspace*:file=$gc_file -cp $callback:$dacapo Harness -c org.sonia.TimeCallback -s $size -n 6 --scratch-directory $scratch_dir $bench 2> $time_log
+        # Prepare directories and log files with run number
+        scratch_dir="$scratch_parent/${bench}_run${run}"
+        gc_file="$gc_parent/${bench}_run${run}.log"
+        time_log="$gc_parent/${bench}_run${run}.time"
 
+        # Create the specific scratch directory for this run
+        mkdir -p "$scratch_dir"
+        touch "$time_log"
+
+        # Run the benchmark
+        $java $java_opts $testing_opts -Xlog:gc*,metaspace*:file="$gc_file" -cp "$callback:$dacapo" Harness -c org.sonia.TimeCallback -s "$size" -n 1 --scratch-directory "$scratch_dir" "$bench" 2> "$time_log"
+    done
 done
-
-
